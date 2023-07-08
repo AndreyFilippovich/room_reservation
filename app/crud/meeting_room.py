cb09a61f68c12f54d1868ab51b1cd551f8b3cf4a
Андрей Филиппович <andrejfilippovic8@gmail.com>
2023-07-08 15:14:36 +0300

    add dependency injection

--- app/crud/meeting_room.py
+++ app/crud/meeting_room.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.core.db import AsyncSessionLocal
 from app.models.meeting_room import MeetingRoom
@@ -10,7 +11,8 @@ from app.schemas.meeting_room import MeetingRoomCreate
 # Функция работает с асинхронной сессией,
 # В функцию передаём схему MeetingRoomCreate.
 async def create_meeting_room(
-        new_room: MeetingRoomCreate
+        new_room: MeetingRoomCreate,
+        session: AsyncSession,
 ) -> MeetingRoom:
     # Конвертируем объект MeetingRoomCreate в словарь.
     new_room_data = new_room.dict()
@@ -19,29 +21,21 @@ async def create_meeting_room(
     # В параметры передаём пары "ключ=значение", для этого распаковываем словарь.
     db_room = MeetingRoom(**new_room_data)
 
-    # Создаём асинхронную сессию через контекстный менеджер.
-    async with AsyncSessionLocal() as session:
-        # Добавляем созданный объект в сессию.
-        session.add(db_room)
-
-        # Записываем изменения непосредственно в БД.
-        await session.commit()
-
-        # Обновляем объект db_room: считываем данные из БД, чтобы получить его id.
-        await session.refresh(db_room)
-    # Возвращаем только что созданный объект класса MeetingRoom.
+    session.add(db_room)
+    await session.commit()
+    await session.refresh(db_room)
     return db_room
 
 
 # Финкция проверяет на уникальность имя переговорки
-async def get_room_id_by_name(room_name: str) -> Optional[int]:
-    async with AsyncSessionLocal() as session:
-        # Получаем объект класса Result.
-        db_room_id = await session.execute(
-            select(MeetingRoom.id).where(
-                MeetingRoom.name == room_name
-            )
+async def get_room_id_by_name(
+        room_name: str,
+        session: AsyncSession,
+        ) -> Optional[int]:
+    db_room_id = await session.execute(
+        select(MeetingRoom.id).where(
+            MeetingRoom.name == room_name
         )
-        # Извлекаем из него конкретное значение.
-        db_room_id = db_room_id.scalars().first()
+    )
+    db_room_id = db_room_id.scalars().first()
     return db_room_id
